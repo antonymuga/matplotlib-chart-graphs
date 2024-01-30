54dc79c3541fa957862d4504e6af9574539158f7
Antony Muga <anthony.ochieng.muga@gmail.com>
2024-01-30 16:59:22 +0300

    Added code for saving a screenshot of plot

--- gas_plot.py
+++ gas_plot.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 my_plt.figure(figsize=(8,6))
 
-my_plt.title("Gas price vs. consumption")
+my_plt.title("Gas price vs. consumption", fontdict={'fontweight':'bold', 'fontsize': 22})
 
 gas_prices = pd.read_csv('gas_prices.csv')
 my_plt.plot(gas_prices.Year, gas_prices.USA, 'r.--', label='USA')
@@ -15,11 +15,13 @@ my_plt.plot(gas_prices.Year, gas_prices['South Korea'], 'b.--', label='South Kor
 # 	if country in countries_to_view:
 # 		my_plt.plot(gas_prices['Year'], gas_prices[country], marker='.')
 
-my_plt.xticks(gas_prices.Year[::3])
+my_plt.xticks(gas_prices.Year[::3].tolist()+[2011])
 
 my_plt.xlabel('Year of Consumption')
 my_plt.ylabel('Gas Price')
 
 my_plt.legend(bbox_to_anchor=(1.0, 0.5), loc='upper left')
+
+my_plt.savefig('Gas_price_graph.png', dpi=300)
 
 my_plt.show()
